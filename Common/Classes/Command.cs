076df01494181e9747664ce4eb6c3c8bdcdf83fd
sequenze <sdmclean3@hotmail.com>
2017-03-18 14:01:50 +0100

    cleanup

--- Common/Classes/Command.cs
+++ Common/Classes/Command.cs
@@ -2,7 +2,7 @@
 using System;
 using System.Collections.Generic;
 
-namespace Common
+namespace Common.Classes
 {
     public class Command
     {
--- RandomWalkClient/Classes/Agent.cs
+++ RandomWalkClient/Classes/Agent.cs
@@ -1,9 +1,5 @@
-﻿using Common;
+﻿using Common.Classes;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RandomWalkClient.Classes
 {
